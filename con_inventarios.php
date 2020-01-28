78c8d4d2bf50f326757b996b62bd5de6ea083d7c
hector384 <60361037+hector384@users.noreply.github.com>
2020-01-28 15:07:36 -0500

    Update con_inventarios.php

--- con_inventarios.php
+++ con_inventarios.php
@@ -334,10 +334,11 @@
                         <input type="reset" class="btn btn-outline-secondary btn-lg" data-dismiss="modal" value="Cancelar">
                         <input type="submit" class="btn btn-primary btn-lg" value="Guardar">
                         <div>
-                        
+
                </div>     
             </div>
         </div></div>
+        <div></div>
     
 
 
