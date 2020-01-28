7c839cc566edea4863d9bdb5c96dbf7720f15c4c
hector384 <60361037+hector384@users.noreply.github.com>
2020-01-28 08:57:36 -0500

    Update conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'divideo_nissi', 'D05m4y4n1t05!', 'divideo_nissi');
+$mysqli = new mysqli('localhost', 'root', '', 'divideo_nissi');
 mysqli_set_charset($mysqli,'utf8');
 if ($mysqli->connect_errno) {
 	echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
