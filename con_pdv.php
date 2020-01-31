c65874e168475f87c5d80fc5f91a4f6d74f43200
hector384 <60361037+hector384@users.noreply.github.com>
2020-01-30 20:21:54 -0500

    Create con_pdv.php

---
+++ con_pdv.php
@@ -0,0 +1,114 @@
+<?php include('includes/conexion.php'); ?>
+<?php include('includes/sesion.php'); 
+?>
+
+<!DOCTYPE html>
+    <html class="loading" lang="en" data-textdirection="ltr"> 
+        <?php include('head.php'); ?>
+            <body class="vertical-layout vertical-compact-menu 2-columns   fixed-navbar" data-open="click" data-menu="vertical-compact-menu" data-col="2-columns">
+            <?php include('header.php'); ?>
+            <?php include('menu.php'); ?> 
+    <!-- INICIO: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+                <div class="content-header-left col-md-6 col-12 mb-2">
+                    <h3 class="content-header-title mb-6">Punto de venta</h3>
+                    <div class="row breadcrumbs-top">
+
+                    </div>
+                </div>
+                <div class="content-header-right text-md-right col-md-6 col-12">
+                    <div class="btn-group">
+                    <a class="btn btn-float btn-float-lg btn-success" style="color:#fff;"><i class="la la-birthday-cake"  data-toggle="modal" data-target="#iconForm"></i><span>Crear Producto</span></a>
+                    <a class="btn btn-float btn-float-lg btn-pink " style="color:#fff;"><i class="la la-plus"  data-toggle="modal" data-target="#iconEntrada"></i><span>Entrada Inventario</span></a>
+                    <a class="btn btn-float btn-float-lg btn-warning" style="color:#fff;"><i class="la la-minus"  data-toggle="modal" data-target="#iconSalida"></i><span>Salida Inventario</span></a>
+                    
+                    </div>
+                </div>
+            </div>
+            <div class="content-body"> <br> <br>
+
+
+    <div class="row">
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;"><br><h3 align="center">item 1</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="bebida.jpg" style="width:80%;height:auto;" > <h3 align="center">item 2</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;" > <h3 align="center">item 3</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="bebida.jpg" style="width:80%;height:auto;" > <br><h3 align="center">item 4</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;" > <br> <h3 align="center">item 5</h3></a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;"><br><h3 align="center">item 6</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="bebida.jpg" style="width:80%;height:auto;" > <br><h3 align="center">item 7</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;" > <br> <h3 align="center">item 8</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="bebida.jpg" style="width:80%;height:auto;" > <h3 align="center">item 9</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;" > <h3 align="center">item 10</h3></a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;"><h3 align="center">item 11</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="bebida.jpg" style="width:80%;height:auto;" > <h3 align="center">item 12</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;" > <h3 align="center">item 13</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="bebida.jpg" style="width:80%;height:auto;" > <h3 align="center">item 14</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;" ><h3 align="center">item 15</h3></a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;"><h3 align="center">item 16</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="bebida.jpg" style="width:80%;height:auto;" > <h3 align="center">item 17</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;" > <h3 align="center">item 18</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="bebida.jpg" style="width:80%;height:auto;" > <br>  <h3 align="center">item 19</h3></a>
+        </div>
+        <div class="col-sm-2 co-md-2 col-lg-2">
+            <a href="#" class="button"><img src="http://localhost/dashboard/app-assets/images/logo/logo.png" style="width:80%;height:auto;" > <h3 align="center">item 20</h3></a>
+        </div>
+    </div>
+    
+
+
+
+
+
+
+    
+    <!-- Fin: Content-->
+        </div>
+    </div>
+</div>
+    <?php include('footer.php'); ?>
